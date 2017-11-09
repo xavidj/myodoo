1f13e1863d33a67f3499055c1d4a417552c9da09
Javier Izco <javier.izco@citylok.com>
2017-11-09 16:33:56 +0100

    empty module people

---
+++ myodoo_people/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2017 Xavidj
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "People",
+    "version": "1.0.0.0.0",
+    "author": "Xavidj",
+    "website": 'https://github.com/xavidj/myodoo.git',
+    "category": "People",
+    "license": "AGPL-3",
+    "depends": [
+    ],
+    "data": [
+    ],
+    'installable': True,
+}
