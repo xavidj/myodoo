2c272a1de9465595694b570e06b0572fe833f7f9
Javier Izco <javier.izco@citylok.com>
2017-11-10 09:41:21 +0100

    Add image to person in kanban view

--- myodoo_people/models/people.py
+++ myodoo_people/models/people.py
@@ -4,11 +4,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import calendar
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class People(models.Model):
     _name = 'myodoo.people'
 
     age = fields.Integer(string='Age')
-
+    name = fields.Char('Name', required=True)
+    avatar = fields.Binary(string='Avatar')
