1c3bddeb2af4a1d1b5655c379f546966fb37c5c8
xavidj <xavi@riojavista.com>
2017-11-14 12:33:09 +0100

    change developers

--- myodoo_people/models/people.py
+++ myodoo_people/models/people.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
-# Copyright 2012-2017 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 import calendar
